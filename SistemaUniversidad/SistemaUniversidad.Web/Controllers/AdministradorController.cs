87d241339842ef613149005c5681eed947d61b33
Federico G <fedegv.4991@gmail.com>
2020-08-27 14:09:20 -0300

    Agrega y termina funciones de administracion de profesores

--- SistemaUniversidad/SistemaUniversidad.Web/Controllers/AdministradorController.cs
+++ SistemaUniversidad/SistemaUniversidad.Web/Controllers/AdministradorController.cs
@@ -18,70 +18,13 @@ namespace SistemaUniversidad.Web.Controllers
             User model = (User)Session["user"];
             return View(model);
         }
-
         public ActionResult Profesores()
         {
             SqlProfesorData sqlProfesor = new SqlProfesorData();
             var listaProfesor = sqlProfesor.ObtenerTodos();
             return View(listaProfesor);
         }
-        [HttpGet]
-        public ActionResult EditarProfesor(int id)
-        {
-            SqlProfesorData sqlProfesor = new SqlProfesorData();
-            Profesor profesor = sqlProfesor.Obtener(id);
-            return View(profesor);
-        }
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult EditarProfesor(Profesor profesor)
-        {
-            if (ModelState.IsValid)
-            {
-                if (profesor != null)
-                {
-                    SqlProfesorData sqlProfesor = new SqlProfesorData();
-                    sqlProfesor.Actualizar(profesor);
-                    return RedirectToAction("Profesores", "Administrador");
-                }
-            }
-                   
-            return View(profesor);
-        }
 
-        public ActionResult EliminarProfesor(int id)
-        {
-            SqlProfesorData sqlProfesor = new SqlProfesorData();
-            Profesor profesor = sqlProfesor.Obtener(id);
-            return View(profesor);
-        }
-        public ActionResult DetallesProfesor(int id)
-        {
-            SqlProfesorData sqlProfesor = new SqlProfesorData();
-            Profesor profesor = sqlProfesor.Obtener(id);
-            return View(profesor);
-        }
-        [HttpGet]
-        public ActionResult CrearProfesor()
-        {
-            return View();
-        }
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult CrearProfesor(Profesor profesor)
-        {
-            if (ModelState.IsValid)
-            {
-                if (profesor != null)
-                {
-                    SqlProfesorData sqlProfesor = new SqlProfesorData();
-                    sqlProfesor.Agregar(profesor);
-                    return RedirectToAction("Profesores", "Administrador");
-                }
-            }
 
-            return View(profesor);
-
-        }
     }
 }
---
+++ SistemaUniversidad/SistemaUniversidad.Web/Controllers/ProfesorController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Universidad.Data.Models;
+using Universidad.Data.Services;
+
+namespace SistemaUniversidad.Web.Controllers
+{
+    public class ProfesorController : Controller
+    {
+        // GET: Profesor
+
+        [HttpGet]
+        public ActionResult Editar(int id)
+        {
+            SqlProfesorData sqlProfesor = new SqlProfesorData();
+            Profesor profesor = sqlProfesor.Obtener(id);
+            return View("~/Views/Profesor/Editar.cshtml", profesor);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Editar(Profesor profesor)
+        {
+            if (ModelState.IsValid)
+            {
+                if (profesor != null)
+                {
+                    SqlProfesorData sqlProfesor = new SqlProfesorData();
+                    sqlProfesor.Actualizar(profesor);
+                    return RedirectToAction("Profesores", "Administrador");
+                }
+            }
+
+            return View(profesor);
+        }
+        [HttpGet]
+        public ActionResult Eliminar(int id)
+        {
+            SqlProfesorData sqlProfesor = new SqlProfesorData();
+            Profesor profesor = sqlProfesor.Obtener(id);
+            return View("~/Views/Profesor/Eliminar.cshtml", profesor);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Eliminar(Profesor profesor)
+        {
+
+            if (profesor != null)
+            {
+                SqlProfesorData sqlProfesor = new SqlProfesorData();
+                sqlProfesor.Remover(profesor.Id);
+                return RedirectToAction("Profesores", "Administrador");
+            }
+
+            return View(profesor);
+        }
+        [HttpGet]
+        public ActionResult Detalles(int id)
+        {
+            SqlProfesorData sqlProfesor = new SqlProfesorData();
+            Profesor profesor = sqlProfesor.Obtener(id);
+            return View("~/Views/Profesor/Detalles.cshtml", profesor);
+        }
+        [HttpGet]
+        public ActionResult Crear()
+        {
+            return View("~/Views/Profesor/Crear.cshtml");
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Crear(Profesor profesor)
+        {
+            if (ModelState.IsValid)
+            {
+                if (profesor != null)
+                {
+                    SqlProfesorData sqlProfesor = new SqlProfesorData();
+                    sqlProfesor.Agregar(profesor);
+                    return RedirectToAction("Profesores", "Administrador");
+                }
+            }
+
+            return View("~/Views/Profesor/Crear.cshtml", profesor);
+
+        }
+    }
+}
